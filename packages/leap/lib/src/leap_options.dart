02f2be0c9d47cf2ce51ee2109fbe21a42dcd7457
Erick <erickzanardoo@gmail.com>
2023-11-15 21:22:18 -0300

    feat: allow useAtlas and layerPaintFactory to be configured through TiledOptions (#27)

--- packages/leap/lib/src/leap_options.dart
+++ packages/leap/lib/src/leap_options.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:tiled/tiled.dart';
 
 /// A configurable class that allows the developer to
@@ -28,6 +29,8 @@ class TiledOptions {
     this.atlasMaxX,
     this.atlasMaxY,
     this.tsxPackingFilter,
+    this.useAtlas = true,
+    this.layerPaintFactory,
   });
 
   /// Which layer name should be used for the player, defaults to "Ground".
@@ -74,4 +77,20 @@ class TiledOptions {
   /// When omitted defaults to Flame Tiled's default filter, which include all
   /// tilesets in the atlas.
   final bool Function(Tileset)? tsxPackingFilter;
+
+  /// A flag that indicates if Flame Tiled should render the map
+  /// using `Canvas.drawAtlas` instead of `Canvas.drawImageRect`.
+  ///
+  /// Defaults to Flame Tiled default value, which is `true`.
+  ///
+  /// Refer to Flame's SpriteBatch documentation for more information
+  /// on the differences between the two methods.
+  final bool useAtlas;
+
+  /// A function that allows the developer to customize the paint
+  /// used to render tile maps
+  ///
+  /// When ommited, resorts to Flame Tiled one, which creates a
+  /// a white Paint with the layer opacity value.
+  final Paint Function(double opacity)? layerPaintFactory;
 }
