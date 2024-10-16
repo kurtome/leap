ebfa27754e18d9b118423290c14cfb33c79f00fc
Kurtis Melby <kurt@melby.me>
2024-10-15 23:22:04 -0400

    feat: adding extensions methods for direction enums (#75)

--- packages/leap/lib/src/utils/direction.dart
+++ packages/leap/lib/src/utils/direction.dart
@@ -3,7 +3,27 @@ enum HorizontalDirection {
   right,
 }
 
+extension HorizontalDirectionExtension on HorizontalDirection {
+  HorizontalDirection flip() {
+    if (this == HorizontalDirection.left) {
+      return HorizontalDirection.right;
+    } else {
+      return HorizontalDirection.left;
+    }
+  }
+}
+
 enum VerticalDirection {
   up,
   down,
+}
+
+extension VerticalDirectionExtension on VerticalDirection {
+  VerticalDirection flip() {
+    if (this == VerticalDirection.up) {
+      return VerticalDirection.down;
+    } else {
+      return VerticalDirection.up;
+    }
+  }
 }
